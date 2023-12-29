607e995327eb4daf3c9d02d2b35ba0afb95b4868
Shaban Khan <90864408+playshaban@users.noreply.github.com>
2023-12-29 15:20:50 +0530

    Update Registration.jsx, backend

--- client/src/page/Registration.jsx
+++ client/src/page/Registration.jsx
@@ -31,7 +31,7 @@ const Registration = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            const response = await fetch(`http://localhost:5000/api/auth/register`,
+            const response = await fetch(`https://mern-website-with-admin-panel.vercel.app/api/auth/register`,
                 {
                     method: "POST",
                     headers:
